1f75a5fa1ed66ee813fd6d0bdcee276204afe0db
VeronikaMi <nika.mitichashili@gmail.com>
2022-07-07 18:13:10 +0400

    started with design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public users: User[] = [
+    {
+      name: 'Michael Holz',
+      position: 'Manager',
+      imgUrl: 'assets/male.png',
+    },
+    {
+      name: 'André Stewart',
+      position: 'Product Manager',
+      imgUrl: 'assets/male.png',
+    },
+    {
+      name: 'Jassy Smith',
+      position: 'UX Designer',
+      imgUrl: 'assets/female.png',
+    },
+    {
+      name: 'George Porter',
+      position: 'Software Engineer',
+      imgUrl: 'assets/male.png',
+    },
+    {
+      name: 'Leanne Graham',
+      position: 'Software Engineer',
+      imgUrl: 'assets/female.png',
+    },
+  ];
+  public onAddNewClick() {}
+
+  public deleteTask() {}
+}
+
+export class User {
+  public name: string = '';
+  public position: string = '';
+  public imgUrl: string = '';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonsModule,
+    BrowserAnimationsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
